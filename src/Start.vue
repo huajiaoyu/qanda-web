3fc14cdeb05ca8d5ce6ac3e3c2de4332ac07288c
huajiaoyu <2847476047@qq.com>
2025-05-05 18:49:52 +0800

    feat(slots): multi-slots

--- src/Start.vue
+++ src/Start.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="save-load-container">
     <!-- 原页面内容 -->
-    <button @click="restart" class="restart-button">重新开始</button>
-    <div v-for="(slot, index) in saveSlots" :key="index" class="save-slot">
-      <span>{{ `存档${index + 1}` }}</span>
+<!--    <button @click="restart" class="restart-button">重新开始</button>-->
+    <div v-for="(bank, index) in question_bank" :key="index" class="save-slot">
+      <span>{{ `${bank}` }}</span>
       <button @click="loadSlot(index)" class="load-button">读取</button>
     </div>
 
@@ -15,7 +15,20 @@
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import apiClient from '@/axios/axios.js'; // 导入配置好的axios实例
 
+const question_bank = ref([])
+
+
+
+const load = async () => {
+  let response
+  response = await apiClient.get('/question/list');
+  question_bank.value = response.data
+  console.log(question_bank.value)
+};
+
+load()
 const router = useRouter();
 
 // 重新开始逻辑
--- src/axios/axios.js
+++ src/axios/axios.js
@@ -5,8 +5,9 @@ import axios from 'axios';
 const apiClient = axios.create({
     // baseURL: "http://127.0.0.1:4523/m1/6211700-5905121-default/api/v1",
     // baseURL: "http://localhost:8080/api/v1",
-    baseURL: "http://192.168.137.1:8080/api/v1",
+    // baseURL: "http://192.168.137.1:8080/api/v1",
     // baseURL: "http://192.168.1.3:8080/api/v1",
+    baseURL: "https://103.8.71.176:18080/api/v1",
     timeout: 10000000,
     headers: {'Content-Type': 'application/json'}
 });
